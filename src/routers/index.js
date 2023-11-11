9051026a6c59a06ccac72edeb89d80f8b0b4b568
jamilhamdihrp <jamil.harahap@iconpln.co.id>
2023-11-12 01:37:22 +0700

    5.0

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,30 +4,32 @@ import accountRoutes from "./user.js";
 import attendanceRoutes from "./attendance.js";
 import questionRouter from "./question.js";
 import divisionRouter from "./devision.js";
-import { scheduleTransaction } from "../helper/scheduleHelper.js";
-// import { CronJob } from 'cron';
+// import { scheduleTransaction } from "../helper/scheduleHelper.js";
+import { CronJob } from 'cron';
 
 const routers = express.Router();
 
 
-const rule = new schedule.RecurrenceRule();
-rule.hour = 6;
-rule.minute = 27;
+// const rule = new schedule.RecurrenceRule();
+// rule.hour = 6;
+// rule.minute = 27;
 
-schedule.scheduleJob(rule, function () {
-  scheduleTransaction();
-  console.log('Scheduled task executed at 12 AM');
-});
+// schedule.scheduleJob(rule, function () {
+//   scheduleTransaction();
+//   console.log('Scheduled task executed at 12 AM');
+// });
 
-// const job = new CronJob('26 6 * * *',
-// 	function () {
-//     scheduleTransaction()
-//     console.log('Scheduled task executed at 1 AM');
-// 	}, 
-// 	null,
-// 	true,
-// 	'Asia/Jakarta'
-// );
+const job = new CronJob('38 6 * * *',
+	function () {
+    scheduleTransaction()
+    console.log('Scheduled task executed at 1 AM');
+	}, 
+	null,
+	true,
+	'UTC'
+);
+
+job.start();
 
 routers.use((req, res, next) => {
   const apiKey = req.header("API-KEY");
