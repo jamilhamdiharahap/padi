7c5ae01cabecf178355ae3820259631d8d427fa5
khalil <jamil.harahap@iconpln.co.id>
2024-04-29 23:51:33 +0700

    3.1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/v1", (_, res) => {
-    res.status(200).json({ message: "Welcome To Padi!" });
+    res.status(200).json({ message: "Welcome To Padi!!" });
 });
 
 app.use(routers);
